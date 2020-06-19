c3af195a7357da99ae56a69c365287f60b70e1fa
kapil.kumar <kapil.kumar@jellyfishtechnologies.com>
2020-06-19 18:24:11 +0530

    Final commit for tasky electron app

--- tasky/index.js
+++ tasky/index.js
@@ -1,41 +1,20 @@
 const path = require('path');
 const electron =  require('electron');
-const { app, BrowserWindow, Tray } = electron;
+const TimmerTray = require('./app/timmer_tray');
+const MainWindow = require('./app/mainWindow');
+const { app, ipcMain } = electron;
 
 let mainWindow;
 let tray;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({
-        height: 500,
-        width: 300,
-        frame: false,
-        resizable: false,
-        show: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-    
+    mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
+
     const iconName = process.platform === 'win32' ? 'iconTemplate.png' : 'windows-icon.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
-    tray = new Tray(iconPath);
-    tray.on('click', (event, bounds) => {
-        const { x, y} = bounds;
-        const {height, width } = mainWindow.getBounds();
+    tray = new TimmerTray(iconPath, mainWindow);
+})
 
-        if(mainWindow.isVisible()){
-            mainWindow.hide();
-        } else {
-            const yPosition = process.platform === 'darwin' ? y : y - height;
-            mainWindow.setBounds({
-                x: 940, //x - width / 2,
-                y : yPosition, 
-                height,
-                width 
-            });
-            mainWindow.show();
-        }
-    })
+ipcMain.on('update-timer', (event, timeLeft) => {
+    tray.setTitle(timeLeft);
 })
